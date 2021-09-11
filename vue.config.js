a1fe73d2cdadf9502f1d7b67024322bf8361d017
lfk29m12 <lfk29m@gmail.com>
2021-09-11 17:45:06 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -5,16 +5,16 @@ const PrerenderSPAPlugin = require('prerender-spa-plugin');
 const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
 module.exports = {
-  publicPath: '/vue/',
-  outputDir: 'docs/vue/',
+  publicPath: '/vue-prerender/',
+  outputDir: 'docs/vue-prerender/',
   configureWebpack(config) {
     if (process.env.NODE_ENV === 'production') {
       config.plugins.push(
         // 創建實例 (第三步)
         new PrerenderSPAPlugin({
           staticDir: path.join(__dirname, 'docs'),
-          indexPath: path.join(__dirname, 'docs/vue/index.html'),
-          routes: ['/vue', '/vue/about'],
+          indexPath: path.join(__dirname, 'docs/vue-prerender/index.html'),
+          routes: ['/vue-prerender', '/vue-prerender/about'],
           renderer: new Renderer({
             renderAfterDocumentEvent: 'render-event',
           }),
