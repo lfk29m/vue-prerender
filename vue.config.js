971f4f134c1a43af71058c2df7fdaba95a9754e1
lfk29m12 <lfk29m@gmail.com>
2021-09-11 14:47:26 +0800

    test

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+// 載入 prerender-spa-plugin (第一步)
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+// 載入 PuppeteerRenderer (第二步)
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+
+module.exports = {
+  configureWebpack(config) {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(
+        // 創建實例 (第三步)
+        new PrerenderSPAPlugin({
+          staticDir: path.join(__dirname, 'dist'),
+          routes: ['/', '/about'],
+          renderer: new Renderer({
+            renderAfterDocumentEvent: 'render-event',
+          }),
+        })
+      );
+    }
+  },
+};
