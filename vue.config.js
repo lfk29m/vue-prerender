4104314b4f5812570c24066a09ae35241ed6c034
lfk29m12 <lfk29m@gmail.com>
2021-09-16 00:10:50 +0800

    tset

--- vue.config.js
+++ vue.config.js
@@ -5,22 +5,21 @@ const PrerenderSPAPlugin = require('prerender-spa-plugin');
 const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
 module.exports = {
-  publicPath: '/',
+  publicPath: '/vue-prerender/',
   outputDir: 'docs',
-  assetsDir: 'vue-prerender',
-  configureWebpack(config) {
-    if (process.env.NODE_ENV === 'production') {
-      config.plugins.push(
-        // 創建實例 (第三步)
-        new PrerenderSPAPlugin({
-          staticDir: path.join(__dirname, 'docs'),
-          indexPath: path.join(__dirname, 'docs/index.html'),
-          routes: ['/vue-prerender', '/vue-prerender/about'],
-          renderer: new Renderer({
-            renderAfterDocumentEvent: 'render-event',
-          }),
-        })
-      );
-    }
-  },
+  // configureWebpack(config) {
+  //   if (process.env.NODE_ENV === 'production') {
+  //     config.plugins.push(
+  //       // 創建實例 (第三步)
+  //       new PrerenderSPAPlugin({
+  //         staticDir: path.join(__dirname, 'docs'),
+  //         indexPath: path.join(__dirname, 'docs/index.html'),
+  //         routes: ['/vue-prerender', '/vue-prerender/about'],
+  //         renderer: new Renderer({
+  //           renderAfterDocumentEvent: 'render-event',
+  //         }),
+  //       })
+  //     );
+  //   }
+  // },
 };
